ebfa4aae9e10f1e892d978a13f446cb859eb8ebd
zikri-muhammad <muhammadzikri598@gmail.com>
2023-09-29 09:01:15 +0700

    feat(database) make connection to mongo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDatabase from './config/database.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // setting up config.env file variables
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
+
+// connect to database
+connectDatabase()
 
 // import all routes
 import routerJob from "./src/routes/jobs.js";
--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -2,8 +2,6 @@ import express from "express";
 import { getJobs } from "../controllers/jobController.js";
 const router = express.Router()
 
-// Import the controller
-
 router.get("/jobs", getJobs);
 
 
